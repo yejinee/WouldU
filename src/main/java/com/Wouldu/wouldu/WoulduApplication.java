32c83db74128dc6e5924c199eed12c2da038f11f
Yejin <kimyj9609@gmail.com>
2022-09-28 15:41:29 +0900

    초기세팅

---
+++ src/main/java/com/Wouldu/wouldu/WoulduApplication.java
@@ -0,0 +1,13 @@
+package com.Wouldu.wouldu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoulduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoulduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Wouldu/wouldu/WoulduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wouldu.wouldu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoulduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
