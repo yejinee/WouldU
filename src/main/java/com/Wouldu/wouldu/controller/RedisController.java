1d3e0ccd242a335f833da417a0505978c4f50398
yujeongmon <yujeongmon@gmail.com>
2022-10-05 13:55:54 +0900

    redis server 연결 테스트 완료

---
+++ src/main/java/com/Wouldu/wouldu/controller/RedisController.java
@@ -0,0 +1,37 @@
+package com.Wouldu.wouldu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RedisController {
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    @PostMapping("/redisTest")
+    public ResponseEntity<?> addRedisKey() {
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        vop.set("yellow", "banana");
+        vop.set("red", "apple");
+        vop.set("green", "watermelon");
+        vop.set("green", "watermelon");
+        System.out.println("addRedisKey");
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/redisTest/{key}")
+    public ResponseEntity<?> getRedisKey(@PathVariable String key) {
+        ValueOperations<String, String> vop = redisTemplate.opsForValue();
+        String value = vop.get(key);
+        System.out.println(key);
+        System.out.println(value);
+        return new ResponseEntity<>(value, HttpStatus.OK);
+    }
+}
